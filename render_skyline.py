e7b1f428ebddfa64631967354cf931d714efeb4c
Andrew Brudnak <brudnak@protonmail.com>
2025-05-14 09:29:10 -0700

    🏙️ skyline

--- render_skyline.py
+++ render_skyline.py
@@ -15,19 +15,19 @@ bpy.ops.import_mesh.stl(filepath=stl_path)
 obj = bpy.context.selected_objects[0]
 obj.name = "Skyline"
 
-# Origin to geometry center
+# Center origin
 bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='BOUNDS')
 bpy.context.view_layer.objects.active = obj
 
-# Slight scale bump
+# Slight upscale
 obj.scale = (1.1, 1.1, 1.1)
 
-# Get object bounds and distance
+# Auto framing
 dims = obj.dimensions
 max_dim = max(dims)
-cam_dist = max_dim * 1.7  # Closer framing
+cam_dist = max_dim * 1.3  # 🎯 Tighter zoom
 
-# Add camera and point it at the object
+# Camera setup
 bpy.ops.object.camera_add(location=(cam_dist, -cam_dist, cam_dist))
 camera = bpy.context.object
 track = camera.constraints.new(type='TRACK_TO')
@@ -36,10 +36,10 @@ track.track_axis = 'TRACK_NEGATIVE_Z'
 track.up_axis = 'UP_Y'
 bpy.context.scene.camera = camera
 
-# Add lighting
+# Lighting
 bpy.ops.object.light_add(type='SUN', location=(cam_dist, -cam_dist, cam_dist * 1.5))
 
-# Set render engine and config
+# Render settings
 scene = bpy.context.scene
 scene.render.engine = 'CYCLES'
 scene.cycles.device = 'CPU'
@@ -48,58 +48,49 @@ scene.render.resolution_x = 1024
 scene.render.resolution_y = 768
 scene.render.image_settings.file_format = 'PNG'
 
-# Set up world
+# World setup
 if not scene.world:
     scene.world = bpy.data.worlds.new("SkylineWorld")
 scene.world.use_nodes = True
 bg_node = scene.world.node_tree.nodes.get("Background") or scene.world.node_tree.nodes.new("ShaderNodeBackground")
 
-# Function: create and assign material
-def make_material(name, base_rgb, neon=False):
+# Material factory
+def make_material(name, base_rgb):
     mat = bpy.data.materials.new(name)
     mat.use_nodes = True
     nodes = mat.node_tree.nodes
     links = mat.node_tree.links
-    for node in nodes: nodes.remove(node)
+    for node in nodes:
+        nodes.remove(node)
 
     output = nodes.new(type='ShaderNodeOutputMaterial')
-
-    if neon:
-        emission = nodes.new(type='ShaderNodeEmission')
-        emission.inputs[0].default_value = (*base_rgb, 1)
-        emission.inputs[1].default_value = 5.0
-        links.new(emission.outputs[0], output.inputs[0])
-    else:
-        bsdf = nodes.new(type='ShaderNodeBsdfPrincipled')
-        bsdf.inputs["Base Color"].default_value = (*base_rgb, 1)
-        bsdf.inputs["Roughness"].default_value = 0.4
-        links.new(bsdf.outputs[0], output.inputs[0])
-
+    bsdf = nodes.new(type='ShaderNodeBsdfPrincipled')
+    bsdf.inputs["Base Color"].default_value = (*base_rgb, 1)
+    bsdf.inputs["Roughness"].default_value = 0.4
+    links.new(bsdf.outputs[0], output.inputs[0])
     return mat
 
-# Assign placeholder to object
-obj.data.materials.append(make_material("Base", (0.5, 0.5, 0.5)))
+# Assign initial material
+obj.data.materials.append(make_material("Placeholder", (0.5, 0.5, 0.5)))
 
-# Render function
-def render_with_theme(bg_rgba, obj_rgb, filename, neon=False):
+# Render theme handler
+def render_with_theme(bg_rgba, obj_rgb, filename):
     bg_node.inputs[0].default_value = bg_rgba
-    mat = make_material("SkylineMaterial", obj_rgb, neon=neon)
+    mat = make_material("SkylineMaterial", obj_rgb)
     obj.data.materials[0] = mat
     scene.render.filepath = os.path.abspath(filename)
     bpy.ops.render.render(write_still=True)
 
 # LIGHT MODE
 render_with_theme(
-    bg_rgba=(1, 1, 1, 1),               # White background
-    obj_rgb=(0.15, 0.15, 0.15),         # Dark gray skyline
-    filename="skyline-light.png",
-    neon=False
+    bg_rgba=(1, 1, 1, 1),                 # white background
+    obj_rgb=(0.12, 0.12, 0.12),           # very dark gray
+    filename="skyline-light.png"
 )
 
 # DARK MODE
 render_with_theme(
-    bg_rgba=(0.01, 0.01, 0.01, 1),      # Nearly black background
-    obj_rgb=(0.0, 0.7, 1.0),            # Neon blue skyline
-    filename="skyline-dark.png",
-    neon=True
+    bg_rgba=(0, 0, 0, 1),                 # black background
+    obj_rgb=(0.0, 0.5, 1.0),              # dark blue
+    filename="skyline-dark.png"
 )
